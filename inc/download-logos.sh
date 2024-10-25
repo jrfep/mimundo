43760f93107c781b68d5b10ff4d80aa47a5a20c3
JR Ferrer-Paris <jr.ferrer.paris@gmail.com>
2024-10-25 18:30:08 -0500

    descargar logos

---
+++ inc/download-logos.sh
@@ -0,0 +1,17 @@
+mkdir -p evnts/logos
+wget https://sventomologia.org/wp-content/uploads/2020/08/cropped-Logo-SVE-200x100-01.png --output-document=evnts/logos/SVE.png
+wget https://api.gbif.org/v1/image/unsafe/https%3A%2F%2Fgithub.com%2FSp2000%2Fcolplus-repo%2Fraw%2Fmaster%2Flogos%2FGloBIS_(GART).png  --output-document=evnts/logos/GART.png
+
+wget 'https://programs.wcs.org/portals/53/simposio/abear_web_IISimposioLogo_c.jpg' --output-document=evnts/logos/abear_web_IISimposioLogo_c.jpg
+
+wget "https://esa2023.org.au/wp-content/uploads/2022/10/ESA2023-logos-01e-02
+.svg" --output-document=evnts/logos/ESA2023-logo.svg
+
+wget "https://images.squarespace-cdn.com/content/v1/59ece4196f4ca3771ef086ff/1512337248275-QD9IXSRO9IF3MXDRO5VI/LACCCB2018%404xminim.png?format=1500w" --output-document=evnts/logos/LACCCB2018-logo.png
+
+wget "https://usbnoticias.usb.ve/wp-content/uploads/2015/11/logo_horizontal1.png" --output-document=evnts/logos/XICVE.png
+
+wget "https://conbio.org/images/content_static_pages/iccb2017_webbnr_950x150.jpg" --output-document=evnts/logos/ICCB_XXVIII.jpg
+
+wget "https://image.isu.pub/180612171155-b1c176a7be3e9036f3eab6d39fc92651/jpg/page_1.jpg" --output-document=evnts/logos/CMP_I_portada.jpg
+
